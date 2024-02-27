d8b6855b36efd7217c720b8b5809bd34f0c0dae8
Victor Lawrence <viclawrence99@gmail.com>
2024-02-27 08:23:04 +0100

    Added price filter and other filter

---
+++ src/components/FilterComp.vue
@@ -0,0 +1,39 @@
+<script setup>
+import { ref } from "vue";
+import PriceFilter from "./PriceFilter.vue";
+const sliderMin = ref(50);
+const sliderMax = ref(80);
+</script>
+<template>
+  <div class="flex flex-col gap-2">
+    <h2 class="text-3xl font-medium my-3">Categories</h2>
+    <span class="flex gap-2">
+      <input type="checkbox" />
+      <label for="Regular-Hamper">Regular Hamper</label>
+    </span>
+    <span class="flex gap-2">
+      <input type="checkbox" />
+      <label for="Christmas-Hamper">Christmas Hamper</label>
+    </span>
+    <span class="flex gap-2">
+      <input type="checkbox" />
+      <label for="Easter-Hamper">Easter Hamper</label>
+    </span>
+    <span class="flex gap-2">
+      <input type="checkbox" />
+      <label for="Luxury-Hamper">Luxury Hamper</label>
+    </span>
+    <span class="flex gap-2">
+      <input type="checkbox" />
+      <label for="Birthday-Hamper">Birthday Hamper</label>
+    </span>
+    <span class="flex gap-2">
+      <input type="checkbox" />
+      <label for="Cards">Cards</label>
+    </span>
+  </div>
+  <div class="price-filter">
+    <h2>Filter by price-filter</h2>
+    <PriceFilter />
+  </div>
+</template>
---
+++ src/components/PriceFilter.vue
@@ -0,0 +1,21 @@
+<script setup>
+import { ref } from "vue";
+
+const value = ref([2000, 50000]);
+</script>
+
+<template>
+  <v-card>
+    <v-card-text>
+      <v-range-slider
+        :thumb-size="30"
+        thumb-color="#fff"
+        v-model="value"
+        tile="true"
+        strict
+      ></v-range-slider>
+    </v-card-text>
+
+    from {{ Math.ceil(value[0]) }} to {{ Math.ceil(value[1]) }}
+  </v-card>
+</template>
